fb2bcebfae67dc223e316cd967ade6f05c019123
CRT_HAO <m831718@gmail.com>
2023-05-21 04:08:48 +0800

    Update Week 13 Practical Class Quiz 1

--- Quiz/Theory Class/CS3005301W13/TS1101/main.cpp
+++ Quiz/Theory Class/CS3005301W13/TS1101/main.cpp
@@ -1,8 +1,15 @@
+// Name: 張皓鈞 (B11030202@mail.ntust.edu.tw)
+// Date: May 21, 2023
+// Last Update: May 21, 2023
+// Problem statement: Class Inheritance
+
 #include "Document.h"
 #include "Email.h"
 #include "File.h"
+
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 // ======================
@@ -11,10 +18,11 @@ using namespace std;
 // object passed in contains keyword as a substring
 // of its text property.
 // ======================
-bool ContainsKeyword(const Document& docObject, string keyword)
+bool ContainsKeyword(const Document &docObject, string keyword)
 {
-	if (docObject.getText().find(keyword) != string::npos) return true;
-	return false;
+    if ( docObject.getText().find(keyword) != string::npos )
+        return true;
+    return false;
 }
 
 // ======================
@@ -22,29 +30,32 @@ bool ContainsKeyword(const Document& docObject, string keyword)
 // ======================
 int main()
 {
-	// Create several test objects
-	Email email1("Body about programming in C++",
-		"Larry", "Curly", "Programming");
-	Email email2("Body about running marathons",
-		"Speedy", "Gonzales", "races");
+    // Create several test objects
+    Email email1("Body about programming in C++",
+                 "Larry", "Curly", "Programming");
+    Email email2("Body about running marathons",
+                 "Speedy", "Gonzales", "races");
 
-	File file1("Contents about some C++ file", "file.txt");
-	File file2("Contents about marathon races", "run.txt");
+    File file1("Contents about some C++ file", "file.txt");
+    File file2("Contents about marathon races", "run.txt");
 
-	cout << "Which contains C++?" << endl;
-	if (ContainsKeyword(email1, "C++")) cout << " Email1" << endl;
-	if (ContainsKeyword(email2, "C++")) cout << " Email2" << endl;
-	if (ContainsKeyword(file1, "C++")) cout << " File1" << endl;
-	if (ContainsKeyword(file2, "C++")) cout << " File2" << endl;
+    cout << "Which contains C++?" << endl;
+    if ( ContainsKeyword(email1, "C++") )
+        cout << " Email1" << endl;
+    if ( ContainsKeyword(email2, "C++") )
+        cout << " Email2" << endl;
+    if ( ContainsKeyword(file1, "C++") )
+        cout << " File1" << endl;
+    if ( ContainsKeyword(file2, "C++") )
+        cout << " File2" << endl;
 
-	// Test our assignment operator
-	file2 = file1;
-	file2.setPathname("c:");
-	cout << "After assignment file2=file1 and file2.setPathname(\"c:\"): "
-		<< endl;
-	cout << "File1's path = " << file1.getPathname() << endl;
-	cout << "File2's path = " << file2.getPathname() << endl;
-	
-	return 0;
+    // Test our assignment operator
+    file2 = file1;
+    file2.setPathname("c:");
+    cout << "After assignment file2=file1 and file2.setPathname(\"c:\"): "
+         << endl;
+    cout << "File1's path = " << file1.getPathname() << endl;
+    cout << "File2's path = " << file2.getPathname() << endl;
+
+    return 0;
 }
-
